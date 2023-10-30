5a8e968e663a491bb612ca6268c21bc9fa2fbd06
Jaku-BB <jaku.bb@outlook.com>
2023-10-30 18:53:04 +0100

    feat: get user input

--- algorithms/introduction/main.py
+++ algorithms/introduction/main.py
@@ -2,4 +2,10 @@ def get_natural_number_sum(number):
     return number > 1 and number + get_natural_number_sum(number - 1) or 1
 
 
-print(get_natural_number_sum(5))
+def main():
+    number = int(input("Liczba naturalna: "))
+    print(get_natural_number_sum(number))
+
+
+if __name__ == '__main__':
+    main()
