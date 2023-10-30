428ee92aea308ea9a8db74419a80b9a09d51f555
Jaku-BB <jaku.bb@outlook.com>
2023-10-30 19:05:25 +0100

    feat: measure execution time

--- algorithms/introduction/main.py
+++ algorithms/introduction/main.py
@@ -1,10 +1,15 @@
+from time import time
+
+
 def get_natural_number_sum(number):
     return number > 1 and number + get_natural_number_sum(number - 1) or 1
 
 
 def main():
     number = int(input("Liczba naturalna: "))
-    print(get_natural_number_sum(number))
+
+    start_time = time()
+    print(f"Wynik: {get_natural_number_sum(number)}\nCzas wykonania: {(time() - start_time) * 1000}")
 
 
 if __name__ == '__main__':
